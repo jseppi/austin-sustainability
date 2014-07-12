fcd112e2d3de10ab8f412725b7b8863dc16fc6fe
James Seppi <james.seppi@gmail.com>
2014-07-12 14:48:44 -0500

    apply body class based on section slug

--- app/scripts/controllers/AppCtrl.js
+++ app/scripts/controllers/AppCtrl.js
@@ -1,9 +1,18 @@
 'use strict';
 
 App
-  .controller('AppCtrl', function($scope) {
-    $scope.myVar = 123;
-    console.log('in AppCtrl'); 
+  .controller('AppCtrl', function ($scope, $state) {
+    $scope.sectionClass = '';
+
+    $scope.$on('$stateChangeSuccess', function() {
+      if ($state.current.data && $state.current.data.section) {
+        //update the bound sectionClass variable to the current slug
+        $scope.sectionClass = $state.current.data.section.slug;
+      }
+      else {
+        $scope.sectionClass = '';
+      }
+    });
 
   });
 
