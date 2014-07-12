2c08c672d36e252c0ecae93e60129fb734207ed4
James Seppi <james.seppi@gmail.com>
2014-07-12 10:02:12 -0500

    use ui-router

--- app/app.js
+++ app/app.js
@@ -1,17 +1,27 @@
 'use strict';
-var App;
-App = angular.module('app', ['ngCookies', 'ngResource', 'ngRoute', 'app.controllers', 'app.directives', 'app.filters', 'app.services']);
-App.config([
-  '$routeProvider', '$locationProvider', function($routeProvider, $locationProvider, config) {
-    $routeProvider.when('/todo', {
-      templateUrl: 'partials/todo.html'
-    }).when('/view1', {
-      templateUrl: 'partials/partial1.html'
-    }).when('/view2', {
-      templateUrl: 'partials/partial2.html'
-    }).otherwise({
-      redirectTo: '/todo'
-    });
-    return $locationProvider.html5Mode(false);
-  }
+var App = angular.module('app', [
+  'ngCookies', 'ngResource', 'ui.router', 
+  'app.controllers', 'app.directives', 'app.filters',
+  'app.services'
 ]);
+
+App.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('start', {
+      url: '/',
+      templateUrl: 'partials/start.html'
+    });
+    // .when('/', {
+    //   templateUrl: 'partials/start.html'
+    // })
+    // .when('/climate_and_energy', {
+    //   templateUrl: 'partials/climate_and_energy.html'
+    // })
+    // //TODO: The other 6 main categories 
+    // .otherwise({
+    //   redirectTo: '/'
+    // });
+
+    $urlRouterProvider.otherwise('/');
+    return;
+});
