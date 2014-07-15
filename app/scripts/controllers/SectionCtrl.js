6597dde32026a8c553317f16bc6d878f868cc223
James Seppi <james.seppi@gmail.com>
2014-07-15 09:53:01 -0500

    yaml for section config and content

--- app/scripts/controllers/SectionCtrl.js
+++ app/scripts/controllers/SectionCtrl.js
@@ -1,36 +1,24 @@
 'use strict';
 
-
-function parseSections (text) {
-  var sections = [];
-  var lines = text.split('\n');
-  var headingRegex = /^\s*(#+)\s(.*)/;
-
-  _.each(lines, function (line) {
-    if (angular.isString(line)) {
-      var match = line.match(headingRegex);
-      if (match) {
-        sections.push({
-          level: match[1].length,
-          heading: match[2]
-        });
-      }
-    }
-  });
-
-  return sections;  
-}
-
 App
   .controller('SectionCtrl', function ($scope, $http, $templateCache, $state, CONTENT_PATH) {
     $scope.section = $state.current.data.section; 
 
     //Path to content file for the currently selected section
-    var contentFilePath = CONTENT_PATH + $scope.section.slug + ".md";
-  
+    var contentFilePath = CONTENT_PATH + $scope.section.slug + ".yml";
     $http.get(contentFilePath, {cache: $templateCache})
       .success(function (response) {
-        console.log(parseSections(response));
-      });
-  });
+        var sectionContent = jsyaml.load(response);
 
+        $scope.sectionLead = sectionContent.lead;
+        $scope.subsections = sectionContent.subsections;
+
+        $scope.selectedSubsection = _.first($scope.subsections);
+      });
+
+
+    $scope.selectSubsection = function (ss) {
+      $scope.selectedSubsection = ss;
+    };
+
+  });
