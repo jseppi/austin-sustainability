13a95ebd40a867d813e30bc50c3d3ece6038bd11
James Seppi <james.seppi@gmail.com>
2014-07-20 21:17:52 -0500

    simplying by moving directive functionality into controller

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -4,4 +4,15 @@ App.controller('HomeCtrl', function ($scope, $state, sections) {
 
   $scope.sections = sections;
 
+  $scope.expandedSection = null;
+  
+  $scope.expandSection = function (section) {
+    if ($scope.expandedSection === section) {
+      $scope.expandedSection = null;
+    }
+    else {
+      $scope.expandedSection = section;
+    }
+  };
+
 });
