d4a59798630077d8b20249a170f88bfdcd529b2f
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-09 14:39:28 -0500

    include a close button for exiting node-webkit app

--- app/scripts/controllers/AppCtrl.js
+++ app/scripts/controllers/AppCtrl.js
@@ -4,6 +4,13 @@ App
   .controller('AppCtrl', function ($scope, $state) {
     $scope.sectionClass = 'home';
 
+    $scope.isNodeWebkit = (typeof process === "object") && process.versions['node-webkit'];
+
+    $scope.closeKiosk = function () {
+      var gui = require('nw.gui');
+      gui.App.quit();
+    };
+
     $scope.$on('$stateChangeSuccess', function() {
       $scope.sectionClass = $state.current.name;
     });
