77908e467f28b74d0c53029ad69c73629539fbba
wilsaj <wilson.andrew.j+github@gmail.com>
2014-08-13 21:40:37 -0500

    zoom via gui.Window for node webkit version this is to accompany the node-webkit HiDPI display settings hack on touchscreen The current node-webkit release doesn't contain a chromium build that natively supports 4k resolution, so just looks pixelated and bad

--- app/scripts/controllers/AppCtrl.js
+++ app/scripts/controllers/AppCtrl.js
@@ -6,6 +6,14 @@ App
 
     $scope.isNodeWebkit = (typeof process === "object") && process.versions['node-webkit'];
 
+    $scope.$watch('isNodeWebkit', function () {
+      if ($scope.isNodeWebkit) {
+        var gui = require('nw.gui');
+        var win = gui.Window.get();
+        win.zoomLevel = 2;
+      }
+    });
+
     $scope.closeKiosk = function () {
       var gui = require('nw.gui');
       gui.App.quit();
